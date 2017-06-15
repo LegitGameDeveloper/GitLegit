5ffead38ac9eb6f1a21d42c1682418a5a7641786
Jahmale_C <jahmale.c@gmail.com>
2017-06-15 13:54:07 +0200

    [mod]: applied activity-fragment structure

--- app/src/main/java/gitter/privacy/gitlegit/MainActivity.java
+++ app/src/main/java/gitter/privacy/gitlegit/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,23 +13,12 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    @BindView(R.id.content)
-    ViewGroup contentView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_view);
+        setContentView(R.layout.base_view);
         ButterKnife.bind(this);
 
         Log.d(TAG, "onCreate: going to do some preparing");
-    }
-
-    @OnClick(R.id.btn_1)
-    public void doButton1(){
-    }
-
-    @OnClick(R.id.btn_2)
-    public void doButton2(){
     }
 }
