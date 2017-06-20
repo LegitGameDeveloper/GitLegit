0789b809548dbaeb3b58e3d4bab8adeb8fd71c1d
Ryan <r.a.lee@outlook.com>
2017-06-20 14:08:18 +0200

    files added and modded

--- app/src/main/java/gitter/privacy/gitlegit/Fragment/JohnIntroFragment.java
+++ app/src/main/java/gitter/privacy/gitlegit/Fragment/JohnIntroFragment.java
@@ -56,6 +56,48 @@ public class JohnIntroFragment extends BaseFragment {
                     break;
                 case 4:
                     ((MainActivity)getActivity()).switchToDifferentScreen(new SearchingForJobFragment(),SearchingForJobFragment.TAG, false);
+
+
+
+                case 5:
+                    ((MainActivity)getActivity()).setStoryText(getString(R.string.john_scenario2_1),"");
+                    break;
+                case 6:
+                    ((MainActivity)getActivity()).switchToDifferentScreen(new PopopQuestionFragment(),PopopQuestionFragment.TAG, false);
+                case 7:
+                    ((MainActivity)getActivity()).setStoryText(getString(R.string.john_scenario2_2),"Supermarket seller Patrick");
+                    break;
+                case 8:
+                    ((MainActivity)getActivity()).setStoryText(getString(R.string.john_scenario2_3),"Supermarket seller Patrick");
+                    break;
+                case 9:
+//                    ((MainActivity)getActivity()).switchToDifferentScreen(new RegisterSupermarketFragment(),RegisterSupermarketFragment.TAG, false);
+                    break;
+                case 10:
+                    ((MainActivity)getActivity()).setStoryText(getString(R.string.john_scenario2_4), "Supermarket seller Patrick");
+                    break;
+                case 11:
+                    ((MainActivity)getActivity()).setStoryText(getString(R.string.john_scenario2_5), "Supermarket seller Patrick");
+                    break;
+
+                case 12:
+                    ((MainActivity)getActivity()).setStoryText(getString(R.string.john_scenario3_1), "John");
+                    break;
+                case 13:
+                    ((MainActivity)getActivity()).setStoryText(getString(R.string.john_scenario3_2), "John");
+                    break;
+
+                case 14:
+//                    question which fast food to visit
+                    break;
+
+                case 15:
+                    ((MainActivity)getActivity()).setStoryText(getString(R.string.john_scenario3_3), "John");
+                    break;
+                case 16:
+                    ((MainActivity)getActivity()).setStoryText(getString(R.string.john_scenario3_3), "Waiter Linda");
+                    break;
+
                 default:
                     Log.e(TAG, "playStory: unknown story counter for introduction given, why u do dis");
             }
---
+++ app/src/main/java/gitter/privacy/gitlegit/Fragment/RegisterSupermarketFragment.java
@@ -0,0 +1,30 @@
+package gitter.privacy.gitlegit.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import gitter.privacy.gitlegit.MainActivity;
+import gitter.privacy.gitlegit.R;
+
+/**
+ * Created by Ryan on 17/06/2017.
+ */
+
+public class RegisterSupermarketFragment extends BaseFragment {
+    public static final String TAG = "PopopQuestionFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        parentView = bindXMLToFragment(R.layout.register_view, inflater, container);
+        ButterKnife.bind(this, parentView);
+        ((MainActivity)getActivity()).setStoryContainerVisible(false);
+
+        return parentView;
+    }
+
+}
